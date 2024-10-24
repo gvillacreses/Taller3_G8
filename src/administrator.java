b90235f8d2b5c462b75bef8395761435e44978bc
Geovanny Villacreses <gdvillac@espol.edu.ec>
2024-10-23 23:08:04 -0500

    administrator class commit

--- src/administrator.java
+++ src/administrator.java
@@ -1,3 +1,20 @@
 public class administrator extends User {
-    
+
+    public void createUserWithRole(String user, String password){
+        //...
+    }
+
+    public void assignPermission(String user){
+        //...
+    }
+
+    public void updateSoftware(){
+        //...
+    }
+
+    public void assignResponsible(Curso curso, Teacher responsible){
+        //...
+    }
+
+
 }
