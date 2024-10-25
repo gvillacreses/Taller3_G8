cf7e2d5e31f75913a853c9dada216b3308ff7972
jorssanc <jorssanc@espol.edu.ec>
2024-10-25 00:02:20 -0500

    Asociaciones 2

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,18 @@
+
+import java.util.ArrayList;
+
 public class Course {
     private String idCourse;
     private String courseName;
     private boolean availableStatus;
+    private Teacher monitor;
+    // se declararon las asociaciones
+
+    private ArrayList<Forum> ownedForums = new ArrayList<>();
+    private ArrayList<Student> registeredStudents = new ArrayList<>();
+    private ArrayList<Student> waitingStudents = new ArrayList<>();
+    private ArrayList<SummativeActivity> summativeActivities = new ArrayList<>();
+
 
 
     //Getters
@@ -13,4 +24,22 @@ public class Course {
     public void setIdCourse(String idCourse){this.idCourse = idCourse;}
     public void setCourseName(String courseName){this.courseName = courseName;}
     public void setAvailableStatus(boolean availableStatus){this.availableStatus = availableStatus;}
+
+    public ArrayList<Forum> getOwnedForums(){
+        return ownedForums;
+    }
+    public ArrayList<Student> getRegisteredStudents(){
+        return registeredStudents;
+    }
+
+    public ArrayList<Student> getWaitingStudents(){
+        return waitingStudents;
+    }
+    public Teacher getMonitor(){
+        return monitor;
+    }
+    public ArrayList<SummativeActivity> getSummativeActivities(){
+        return summativeActivities;
+    }
+    
 }
